53ab2b1642d40bc0945079f26c25bd6b6e1f651b
Joseph-Cha <joseph.c@kakao.com>
2021-02-23 21:49:46 +0900

    문제 풀이_21.02.23

--- Program.cs
+++ Program.cs
@@ -1,24 +1,17 @@
 ﻿/*
-8383번
-n이 주어졌을 때, 1부터 n까지 합을 구하는 프로그램을 작성하시오.
+번
+
 */
 
 /*
-첫째 줄에 n (1 ≤ n ≤ 10,000)이 주어진다.
+
 */
-using System.IO;
-using System.Collections.Generic;
+
 
 class Program
 {
     static void Main()
     {
-        string input = System.Console.ReadLine();
-        int repetition = int.Parse(input);
 
-
-        for (int i = 0; i < repetition; i++)
-        {
-        }
     }          
 }
