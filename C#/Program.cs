037b8e44421a08d2f12179ffe6552a67193d3b74
Joseph-Cha <75019048+Joseph-Cha@users.noreply.github.com>
2021-05-09 22:34:46 +0900

    파일 이주 및 C++ 개발 환경 셋팅

---
+++ C#/Program.cs
@@ -0,0 +1,53 @@
+﻿/*
+4344번
+입력
+첫째 줄에는 테스트 케이스의 개수 C가 주어진다.
+
+둘째 줄부터 각 테스트 케이스마다 학생의 수 N(1 ≤ N ≤ 1000, N은 정수)이 첫 수로 주어지고, 
+이어서 N명의 점수가 주어진다. 점수는 0보다 크거나 같고, 100보다 작거나 같은 정수이다.
+*/
+
+/*
+출력
+각 케이스마다 한 줄씩 평균을 넘는 학생들의 비율을 반올림하여 소수점 셋째 자리까지 출력한다.
+*/
+using System;
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        for(int i = 0; i < n; i++)
+        {
+            string result = GetAverageRatio(Console.ReadLine());
+            System.Console.WriteLine(result + '%');
+        }
+    }  
+    private static string GetAverageRatio(string data)
+    {
+        string[] array = data.Split(' ');
+
+        int n = array.Length - 1;
+        int sum = 0;
+
+        // 평균 산출
+        for(int i = 1; i < n + 1; i++)
+        {
+            sum += int.Parse(array[i]);
+        }
+
+        // 평균을 넘는 비율
+        float average = (float)sum / n;
+        int count = 0;
+        for(int i = 1; i < n + 1; i++)
+        {
+            if((float)int.Parse(array[i]) > average)
+                count++;
+        }
+        float percent = ((float)count / n) * 100;;
+        percent = (float)Math.Round(percent, 3, MidpointRounding.ToEven);
+        string result = string.Format("{0:0.000#}", percent);
+
+        return result;
+    }        
+}
