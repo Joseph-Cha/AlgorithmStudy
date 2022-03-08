4ac782d11fafa75f208c030361b4268b268063fc
Donghun Cha <75019048+Joseph-Cha@users.noreply.github.com>
2022-03-08 22:33:13 +0900

    2, 3, 4번 문제 풀이

--- 인프런_알고리즘_강의/KTW/AA/AA.cpp
+++ 인프런_알고리즘_강의/KTW/AA/AA.cpp
@@ -1,10 +1,16 @@
 ﻿/*
 문제)
-자연수 N이 입력되면 1부터 N까지의 수 중 M의 배수합을 출력하는 프로그램을 작성하세요.
+N(2<=N<=100)명의 나이가 입력됩니다. 이 N명의 사람 중 가장 나이차이가 많이 나는 경우는
+몇 살일까요? 최대 나이 차이를 출력하는 프로그램을 작성하세요.
+
+예시)
+10
+13 15 34 23 45 65 33 11 26 42
+
+54
 */
 
 #include <iostream>
-#define _CRT_SECURE_NO_WARNINGS
 
 using namespace std;
 
@@ -13,15 +19,22 @@ int main()
 	//FILE* fp = nullptr;
 	//freopen_s(&fp, "input.txt", "rt", stdin);
 
-	int n, m, sum = 0;
-	cin >> n >> m;
-	for (int i = 1; i <= n; i++)
+	int n, age = 0;
+	int max = -2000000000;
+	int min = 2000000000;
+
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
 	{
-		if (i % m == 0)
-			sum += i;
+		cin >> age;
+		if (max < age)
+			max = age;
+		if (min > age)
+			min = age;
 	}
 
-	cout << sum;
+	cout << max - min;
 
 	return 0;
 }
