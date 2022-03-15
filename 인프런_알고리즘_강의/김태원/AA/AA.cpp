a3462edd77a28b89888b271416431e91d4b4f4be
Donghun Cha <75019048+Joseph-Cha@users.noreply.github.com>
2022-03-15 22:52:31 +0900

    폴더 정리

--- 인프런_알고리즘_강의/김태원/AA/AA.cpp
+++ 인프런_알고리즘_강의/김태원/AA/AA.cpp
@@ -1,45 +1,23 @@
-﻿/*
-문제)
-괄호가 입력되면 올바른 괄호이면 “YES", 올바르지 않으면 ”NO"를 출력합니다.
-(())() 이것은 괄호의 쌍이 올바르게 위치하는 거지만, (()()))은 올바른 괄호가 아니다.
-
-예시)
-(()(()))(()
-
-NO
-*/
-
-#include <iostream>
-#include <sstream>
+﻿#include <iostream>
 
 using namespace std;
 
-int main() 
+int main()
 {
-	//FILE* fp = nullptr;
-	//freopen_s(&fp, "input.txt", "rt", stdin);
+	cout << "Hello World!" << endl;
+	/*FILE* fp = nullptr;
+	freopen_s(&fp, "input.txt", "rt", stdin);
 
-	char word[30];
-	cin >> word;
-	int a = 0, b = 0;
-	bool success = false;
-	if (word[0] != '(' || word[strlen(word) - 1] != ')')
+	int n = 0;
+	cin >> n;
+	for (int i = 1; i <= n; i++)
 	{
-		success = false;
-		cout << "NO";
-		return 0;
-	}
-
-	for (int i = 0; word[i] != '\0'; i++)
-	{
-		if (word[i] == '(')
-			a++;
-		else
-			b++;
-		if (a == b) success = true;
-		else success = false;
-	}
-
-	success ? cout << "YES" : cout << "NO";
-	return 0;
+		int count = 0;
+		for (int j = 1; j <= i; j++)
+		{
+			if (i % j == 0)
+				count++;
+		}
+		cout << count;
+	}*/
 }
