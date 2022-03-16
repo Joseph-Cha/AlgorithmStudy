e74e0158f23ea613c69e13f4d63c295d2698c2af
Donghun Cha <75019048+Joseph-Cha@users.noreply.github.com>
2022-03-16 22:05:28 +0900

    9번 문제 풀이

--- 인프런_알고리즘_강의/김태원/AA/AA.cpp
+++ 인프런_알고리즘_강의/김태원/AA/AA.cpp
@@ -1,23 +1,39 @@
-﻿#include <iostream>
+﻿// 약수 a의 갯수 => n에서 a의 배수의 갯수 
+// ex n == 8
+// 1 1			=> 1
+// 2 1 1		=> 2
+// 3 1 1		=> 2
+// 4 1 1 1		=> 3
+// 5 1 1		=> 2
+// 6 1 1 1 1	=> 4
+// 7 1 1		=> 2
+// 8 1 1 1 1	=> 4
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main()
 {
-	cout << "Hello World!" << endl;
-	/*FILE* fp = nullptr;
-	freopen_s(&fp, "input.txt", "rt", stdin);
+	//FILE* fp = nullptr;
+	//freopen_s(&fp, "input.txt", "rt", stdin);
 
-	int n = 0;
+	int n;
 	cin >> n;
+	vector<int> a;
+	a.resize(n + 1);
+
 	for (int i = 1; i <= n; i++)
 	{
-		int count = 0;
-		for (int j = 1; j <= i; j++)
+		for (int j = i; j <= n; j += i)
 		{
-			if (i % j == 0)
-				count++;
+			a[j]++;
 		}
-		cout << count;
-	}*/
+
+		cout <<  a[i];
+		if (i != n)
+			cout << " ";
+	}
+
+	return 0;
 }
