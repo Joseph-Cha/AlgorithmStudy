590f984fde6ba4b364730e3d8e7a9e1691cbb6bd
Donghun Cha <75019048+Joseph-Cha@users.noreply.github.com>
2022-03-20 23:25:22 +0900

    13번 문제 풀이

--- 인프런_알고리즘_강의/김태원/AA/AA.cpp
+++ 인프런_알고리즘_강의/김태원/AA/AA.cpp
@@ -1,7 +1,4 @@
-﻿// 10 ~ 15 = 15 - 10 + 1
-// 100 150 = 150 - 100 + 1
-
-#include <iostream>
+﻿#include <iostream>
 #include <vector>
 
 using namespace std;
@@ -10,23 +7,32 @@ int main()
 {
 	//FILE* fp = nullptr;
 	//freopen_s(&fp, "input.txt", "rt", stdin);
-
-	int n;
+	char n[101];
 	cin >> n;
+	int a[10] = {0};
+
+	for (int i = 0; n[i] != '\0'; i++) {
+		int b = n[i];
+		int digit = n[i] - 48;
+		a[digit]++;
+	}
+
+	int sum = -10;
 	int count = 0;
-	int temp = n;
-	while (n > 0) {
-		n = n / 10;
-		count++;
-	}
-	long long sum = 0;
-	for (int i = 1; i <= count - 1; i++) {
-		sum += i * (9 * (pow(10, i - 1)));
+	for (int i = 0; i < 10; i++) {
+		if (a[i] > sum) {
+			sum = a[i];
+			count = i;
+		}
+		else if (a[i] == sum) {
+			if (i > count) {
+				sum = a[i];
+				count = i;
+			}
+		}
 	}
 
-	int a = (temp - pow(10, count - 1) + 1);
-	sum += a * count;
+	cout << count;
 
-	cout << sum;
 	return 0;
 }
