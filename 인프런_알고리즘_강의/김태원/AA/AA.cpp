67375ce0cd06cd257c5cc471d18e53691e3ef822
Donghun Cha <75019048+Joseph-Cha@users.noreply.github.com>
2022-04-27 23:26:10 +0900

    38번 풀이 전략 이해 완료

---
+++ 인프런_알고리즘_강의/김태원/AA/AA.cpp
@@ -0,0 +1,15 @@
+﻿// Inversion Sequence 
+
+// 오늘은 일단 풀이 전략까지만 정리
+// 1 ~ n까지 순서대로 있는 수열에서 뒤에서부터 차근 차근 Inversion Sequence의 값만큼
+// 본인보다 뒤에 있는 값을 앞으로 땡겨오면 된다.
+
+#include <iostream> 
+#include <vector>
+using namespace std;
+int main()
+{
+	FILE* fp = nullptr;
+	freopen_s(&fp, "input.txt", "rt", stdin);
+	return 0;
+}
